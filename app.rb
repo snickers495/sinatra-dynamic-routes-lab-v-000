23b035c4aa1091c148388370379996ab2a3036a2
Lisa Lee <snickers495@gmail.com>
2017-11-03 16:53:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,6 +23,8 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
   end
 end
